f486b1d4c1b75ab275571fa322ab48926b0fe82a
Kevin McDonnell <kevinmcdonnell.jmss@gmail.com>
2024-11-07 15:11:24 -0500

    minor fix

--- load_files.py
+++ load_files.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import re
 import pickle
 
+
 # NB this may not work for all mzml files!!!
 class Spectrum:
 
@@ -29,9 +30,10 @@ class Spectrum:
         self.RT = scan['scanList']['scan'][0]["scan start time"]
         self.mz = scan["m/z array"]
         self.intens = scan["intensity array"]
+        self.scanwindow = [scan["scanList"]["scan"][0]["scanWindowList"]["scanWindow"][0][i] for i in ["scan window lower limit","scan window upper limit"]]
+        
         if self.level==2:
             self.collision_energy = scan["precursorList"]["precursor"][0]["activation"]["collision energy"]
-            self.scanwindow = [scan["scanList"]["scan"][0]["scanWindowList"]["scanWindow"][0][i] for i in ["scan window lower limit","scan window upper limit"]]
             isolationWindow = scan["precursorList"]["precursor"][0]["isolationWindow"]
             self.prec_mz = isolationWindow["isolation window target m/z"]
             self.ms1window = isolationWindow["isolation window target m/z"]+np.array([-1,1])*[isolationWindow['isolation window lower offset'],isolationWindow['isolation window upper offset']]
@@ -54,15 +56,28 @@ class SpectrumFile:
         self.filename = mzml_file
         
         # this may need to be optimised better
+        self.scan_pos = {}
         self.ms1scans = []
         self.ms2scans = []
         with mzml.MzML(mzml_file) as reader:
             for scan in reader:
                 if scan["ms level"] == 1:
-                    self.ms1scans.append(Spectrum(scan))
+                    spec = Spectrum(scan)
+                    self.ms1scans.append(spec)
+                    self.scan_pos[spec.scan_num] = [scan["ms level"],len(self.ms1scans)-1]
                 if scan["ms level"] == 2:
-                    self.ms2scans.append(Spectrum(scan))
-           
+                    spec = Spectrum(scan)
+                    self.ms2scans.append(spec)
+                    self.scan_pos[spec.scan_num] = [scan["ms level"],len(self.ms2scans)-1]
+                
+    
+    def get_by_idx(self,idx):
+        level, level_idx = self.scan_pos[idx]
+        if level==1:
+            return self.ms1scans[level_idx]
+        elif level==2:
+            return self.ms2scans[level_idx]
+            
 def loadSpectra(mzml_file):
     print("Loading Spectra",end=" ")
     python_spec_file = mzml_file+"_pythonspec"
