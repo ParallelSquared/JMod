e7bdf88f2c4402233b5a9763c4ca0b8922240523
Eunice Koo <koo.e@northeastern.edu>
2026-07-23 09:57:23 -0400

    plexdia commit

--- src/run_jmod.py
+++ src/run_jmod.py
@@ -55,7 +55,6 @@ def main(GUI_config_json=None, GUI_result_queue=None):
         if not config.load_config_from_json(config.args.config_json):
             pass
 
-    # A tag implies plexDIA, mirroring the inference the GUI already performs
     if config.args.tag != "None":
         config.args.plexDIA = True
 
