ea9224ee0d51a70312440f94c6cfa2e91200af6b
Eunice Koo <98850825+euniceseo@users.noreply.github.com>
2026-08-20 14:31:15 -0400

    column renamed from coeffs to coeff

--- src/fdr_analysis.py
+++ src/fdr_analysis.py
@@ -1312,7 +1312,7 @@ def process_data(file,spectra,library,mass_tag=None,timeplex=False,SILAC=None,el
 
     ### select minimum columns for parquet
     parquet_columns = ["stripped_seq","z","untag_prec","file_name","channel","is_decoy","Qvalue", "Protein_Qvalue","PredVal",
-                       "protein",'BestChannel_Qvalue', 'plex_Area', 'seq', 'silac_channel', 'untag_seq',"rt","mz","coeffs"]
+                       "protein",'BestChannel_Qvalue', 'plex_Area', 'seq', 'silac_channel', 'untag_seq',"rt","mz","coeff"]
     parquet_columns = [i for i in parquet_columns if i in fdx_quant.columns]
     fdx_quant[parquet_columns].to_parquet(results_folder+"/outputs/all_IDs_filtered.parquet")
 
